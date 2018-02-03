da0e8404d3b2d6e885d3475503b76e777069fea0
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-02-03 15:17:07 +0900

    first commit

---
+++ gui.py
@@ -0,0 +1,179 @@
+import tkinter
+import tkinter.ttk
+import tkinter.font
+
+from strage import g_strage
+
+class Application(object):
+
+    def __init__(self):
+        self.__root = tkinter.Tk()
+        self.__root.title('Vehicle Selector')
+
+        font = tkinter.font.Font(self.__root, family='Arial', size=11, weight='normal')
+        self.__root.option_add('*Font', font)
+
+        self.buildLayout()
+
+        self.__root.mainloop()
+
+
+    def buildLayout(self):
+        vehicleSelectorBar = tkinter.Entry(self.__root, borderwidth=0)
+        vehicleSelectorBar.pack(side='top', expand=1, fill='x')
+
+        moduleSelectorBar = tkinter.Entry(self.__root, borderwidth=0)
+        moduleSelectorBar.pack(side='top', expand=1, fill='x')
+
+        itemPanel = tkinter.Entry(self.__root, borderwidth=8, background='white', foreground='white', relief='flat')
+        itemPanel.pack(side='top', expand=1, fill='x', padx=0, pady=0)
+        
+        self.__nationSelector = DropdownList(vehicleSelectorBar)
+        self.__nationSelector.setWidth(10)
+        self.__nationSelector.setValues(*g_strage.fetchNationList())
+        self.__nationSelector.setCallback(self.cbChangeVehicleFilter)
+
+        self.__tierSelector = DropdownList(vehicleSelectorBar)
+        self.__tierSelector.setWidth(4)
+        self.__tierSelector.setValues(*g_strage.fetchTierList())
+        self.__tierSelector.setCallback(self.cbChangeVehicleFilter)
+
+        self.__typeSelector = DropdownList(vehicleSelectorBar)
+        self.__typeSelector.setValues(*g_strage.fetchTypeList())
+        self.__typeSelector.setWidth(10)
+        self.__typeSelector.setCallback(self.cbChangeVehicleFilter)
+
+        self.__vehicleSelector = DropdownList(vehicleSelectorBar)
+        self.__vehicleSelector.setWidth(40)
+        self.__vehicleSelector.setCallback(self.cbChangeVehicle)
+
+        self.__chassisSelector = DropdownList(moduleSelectorBar)
+        self.__chassisSelector.setWidth(40)
+        self.__chassisSelector.setCallback(self.cbChangeModules)
+
+        self.__turretSelector = DropdownList(moduleSelectorBar)
+        self.__turretSelector.setWidth(40)
+        self.__turretSelector.setCallback(self.cbChangeModules)
+
+        self.__gunSelector = DropdownList(moduleSelectorBar)
+        self.__gunSelector.setWidth(40)
+        self.__gunSelector.setCallback(self.cbChangeModules)
+
+        self.__item = {}
+        self.__item['vehicle'] = PanelItem(itemPanel, 'Vehicle:', '', labelWidth=8, valueWidth=40, valueAnchor='w')
+        self.__item['chassis'] = PanelItem(itemPanel, 'Chassis:', '', labelWidth=8, valueWidth=40, valueAnchor='w')
+        self.__item['turret'] = PanelItem(itemPanel, 'Turret:', '', labelWidth=8, valueWidth=40, valueAnchor='w')
+        self.__item['gun'] = PanelItem(itemPanel, 'Gun:', '', labelWidth=8, valueWidth=40, valueAnchor='w')
+        self.__item['shotDispersionRadius'] = PanelItem(itemPanel, 'ShotDispersionRadius:', ' (m)')
+        PanelItem(itemPanel, 'DispersionFactor', '', labelAnchor='w')
+        self.__item['vehicleMovement'] = PanelItem(itemPanel, '... VehicleMovement:', '')
+        self.__item['vehicleRotation'] = PanelItem(itemPanel, '... VehicleRotation:', '')
+        self.__item['turretRotation'] = PanelItem(itemPanel, '... TurretRotation:', '')
+        self.__item['afterShot'] = PanelItem(itemPanel, '... AfterShot:', '')
+        self.__item['whileGunDamaged'] = PanelItem(itemPanel, '... WhileGunDamaged:', '')
+
+        self.changeVehicleFilter()
+        self.changeVehicle()
+        self.changeModules()
+
+    def changeVehicleFilter(self):
+        nation = self.__nationSelector.getSelected()
+        tier = self.__tierSelector.getSelected()
+        type = self.__typeSelector.getSelected()
+        vehicles = g_strage.fetchVehicleList(nation, tier, type)
+        if not vehicles[0]:
+            vehicles = [ [ '' ], [ None ] ]
+        self.__vehicleSelector.setValues(*vehicles)
+        self.changeVehicle()
+
+    def changeVehicle(self):
+        vehicleId = self.__vehicleSelector.getSelected()
+        if vehicleId:
+            vehicleInfo = g_strage.fetchVehicleInfo(vehicleId)
+            self.__chassisSelector.setValues(*g_strage.fetchChassisList(vehicleId))
+            self.__turretSelector.setValues(*g_strage.fetchTurretList(vehicleId))
+            self.__gunSelector.setValues(*g_strage.fetchGunList(vehicleId))
+            self.changeModules()
+        else:
+            self.__vehiclePanel['text'] = ''
+
+    def changeModules(self):
+        vehicleId = self.__vehicleSelector.getSelected()
+        if vehicleId:
+            vehicleInfo = g_strage.fetchVehicleInfo(vehicleId)
+            self.__item['vehicle'].setValue('{} ({})'.format(vehicleInfo['name'], vehicleInfo['id']))
+        chassisTag = self.__chassisSelector.getSelected()
+        turretTag = self.__turretSelector.getSelected()
+        gunTag = self.__gunSelector.getSelected()
+        print(vehicleId, chassisTag, turretTag, gunTag)
+        chassis = g_strage.fetchChassisInfo(vehicleId, chassisTag)
+        turret = g_strage.fetchTurretInfo(vehicleId, turretTag)
+        gun = g_strage.fetchGunInfo(vehicleId, gunTag)
+        if chassis:
+            self.__item['chassis'].setValue('{} ({})'.format(chassis['name'], chassis['tag']))
+            self.__item['turret'].setValue('{} ({})'.format(turret['name'], turret['tag']))
+            self.__item['gun'].setValue('{} ({})'.format(gun['name'], gun['tag']))
+            self.__item['shotDispersionRadius'].setValue(gun['shotDispersionRadius'])
+            self.__item['vehicleMovement'].setValue(chassis['vehicleMovement'])
+            self.__item['vehicleRotation'].setValue(chassis['vehicleRotation'])
+            self.__item['turretRotation'].setValue(gun['turretRotation'])
+            self.__item['afterShot'].setValue(gun['afterShot'])
+            self.__item['whileGunDamaged'].setValue(gun['whileGunDamaged'])
+    
+    def cbChangeVehicleFilter(self, event):
+        self.changeVehicleFilter()
+
+    def cbChangeVehicle(self, event):
+        self.changeVehicle()
+
+    def cbChangeModules(self, event):
+        self.changeModules()
+
+
+class PanelItem(object):
+    def __init__(self, parent, label, unit, labelWidth=20, labelAnchor='e', valueWidth=6, valueAnchor='e'):
+        self.__panel = tkinter.Entry(parent)
+        self.__panel['borderwidth'] = 0
+        self.__panel.pack(side='top', expand=1, fill='x', pady=1)
+        self.__label = tkinter.Label(self.__panel, width=labelWidth, anchor=labelAnchor, borderwidth=0, background='white')
+        self.__label['text'] = label
+        self.__label.pack(side='left')
+        self.__value = tkinter.Label(self.__panel, width=valueWidth, anchor=valueAnchor, borderwidth=0, background='white')
+        self.__value.pack(side='left')
+        self.__unit = tkinter.Label(self.__panel, width=5, anchor='w', borderwidth=0, background='white')
+        self.__unit['text'] = unit
+        self.__unit.pack(side='left')
+
+    def setValue(self, value):
+        self.__value['text'] = value
+
+class DropdownList(object):
+    __values = [ '' ]
+
+    def __init__(self, parent):
+        self.__combobox = tkinter.ttk.Combobox(parent, state='readonly')
+        self.__combobox.pack(side='left')
+
+    def setWidth(self, value):
+        self.__combobox['width'] = value
+
+    def setCallback(self, cbFunc):
+        self.__combobox.bind('<<ComboboxSelected>>', cbFunc)
+
+    def setValues(self, labels, values):
+        self.__values = values
+        self.__combobox['values'] = labels
+        self.__combobox.current(0)
+
+    def getSelected(self):
+        index = self.__combobox.current()
+        return self.__values[index]
+
+
+
+if __name__ == '__main__':
+    import io, sys
+    sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+    g_app = Application()
---
+++ strage.py
@@ -0,0 +1,177 @@
+import os
+import gettext
+from xml.etree import ElementTree
+
+BASE_DIR = 'c:/Git/wot.scripts/scripts/item_defs/vehicles'
+LOCALE_DIR = 'C:/Games/World_of_Tanks_EU/res'
+
+NATIONS = [ 'germany', 'ussr', 'usa', 'uk', 'france', 'china', 'japan', 'czech', 'sweden', 'poland' ]
+TIERS = [ str(tier) for tier in range(1, 10 + 1) ]
+TYPES = [ 'lightTank', 'mediumTank', 'heavyTank', 'AT-SPG', 'SPG' ]
+
+TYPES_LABEL = { 'lightTank':'LT', 'mediumTank':'MT', 'heavyTank':'HT', 'AT-SPG':'TD', 'SPG':'SPG' }
+TYPES_LIST = [ TYPES_LABEL[t] for t in TYPES ]
+
+_currentDomain = None
+_translation = None
+
+def translate(text):
+    global _currentDomain
+    global _translation
+    if not text[0] == '#':
+        return text
+    prefix, name = text.split(':')
+    if not _currentDomain == prefix.replace('#', ''):
+        _currentDomain = prefix.replace('#', '')
+        _translation = gettext.translation(_currentDomain, languages=['text'], localedir=LOCALE_DIR)
+        _translation.install()
+    name = _(name)
+    return name
+
+
+
+def getEntryTextSafe(e):
+    if e is not None:
+        return e.text
+    return ''
+
+
+class Strage(object):
+
+    def __init__(self):
+        self.__strage = {}
+        self.__strageSharedGun = {}
+        for nation in NATIONS:
+            root = ElementTree.parse(os.path.join(BASE_DIR, nation, 'list.xml')).getroot()
+            for child in root:
+                entry = {}
+                id = nation + '/' + child.tag
+                entry['id'] = id
+                entry['tag'] = child.tag
+                entry['nation'] = nation
+                entry['tier'] = child.find('level').text
+                entry['order'] = int(child.find('id').text)
+                entry['type'] = [ TYPES_LABEL[t] for t in child.find('tags').text.split(' ') if t in TYPES ][0]
+                entry['userString'] = child.find('userString').text
+                entry['name'] = translate(entry['userString'])
+                self.__strage[id] = entry
+            root = ElementTree.parse(os.path.join(BASE_DIR, nation, 'components/guns.xml')).getroot()
+            for gun in root.find('shared'):
+                entry = {}
+                id = nation + '/' + gun.tag
+                entry['id'] = id
+                entry['tag'] = gun.tag
+                entry['nation'] = nation
+                entry['userString'] = gun.find('userString').text
+                entry['shotDispersionRadius'] = gun.find('shotDispersionRadius').text
+                shotDispersionFactors = gun.find('shotDispersionFactors')
+                entry['turretRotation'] = shotDispersionFactors.find('turretRotation').text
+                entry['afterShot'] = shotDispersionFactors.find('afterShot').text
+                entry['whileGunDamaged'] = shotDispersionFactors.find('whileGunDamaged').text
+                self.__strageSharedGun[id] = entry
+
+    def getSharedGunEntry(self, entry, target, nation, gunTag, label):
+        if entry is not None:
+            result = entry.find(target)
+            if result is not None:
+                return result.text
+        id = nation + '/' + gunTag
+        return self.__strageSharedGun[id][label]
+                
+    def fetchVehicleInfo(self, id):
+        nation, tag = id.split('/')
+        root = ElementTree.parse(os.path.join(BASE_DIR, nation, tag + '.xml')).getroot()
+        entry = { k:v for k,v in self.__strage[id].items() }
+        entry['chassis'] = {}
+        entry['chassisList'] = []
+        for chassis in root.find('chassis'):
+            desc = {}
+            desc['tag'] = chassis.tag
+            desc['userString'] = chassis.find('userString').text
+            desc['name'] = translate(desc['userString'])
+            shotDispersionFactors = chassis.find('shotDispersionFactors')
+            desc['vehicleMovement'] = shotDispersionFactors.find('vehicleMovement').text
+            desc['vehicleRotation'] = shotDispersionFactors.find('vehicleRotation').text
+            entry['chassis'][desc['tag']] = desc
+            entry['chassisList'].append(desc['tag'])
+        entry['turret'] = {}
+        entry['turretList'] = []
+        entry['gun'] = {}
+        entry['gunList'] = []
+        for turret in root.find('turrets0'):
+            desc = {}
+            desc['tag'] = turret.tag
+            desc['userString'] = turret.find('userString').text
+            desc['name'] = translate(desc['userString'])
+            desc['guns'] = [ e.tag for e in turret.find('guns') ]
+            entry['turret'][desc['tag']] = desc
+            entry['turretList'].append(desc['tag'])
+            for gun in turret.find('guns'):
+                gunDesc = {}
+                gunDesc['tag'] = gun.tag
+                gunDesc['userString'] = self.getSharedGunEntry(gun, 'userString', nation, gun.tag, 'userString')
+                gunDesc['name'] = translate(gunDesc['userString'])
+                gunDesc['shotDispersionRadius'] = self.getSharedGunEntry(gun, 'shotDispersionRadius', nation, gun.tag, 'shotDispersionRadius')
+                shotDispersionFactors = gun.find('shotDispersionFactors')
+                gunDesc['turretRotation'] = self.getSharedGunEntry(shotDispersionFactors, 'turretRotation', nation, gun.tag, 'turretRotation')
+                gunDesc['afterShot'] = self.getSharedGunEntry(shotDispersionFactors, 'afterShot', nation, gun.tag, 'afterShot')
+                gunDesc['whileGunDamaged'] = self.getSharedGunEntry(shotDispersionFactors, 'whileGunDamaged', nation, gun.tag, 'whileGunDamaged')
+                entry['gun'][gunDesc['tag']] = gunDesc
+                entry['gunList'].append(gunDesc['tag'])
+        return entry
+
+    def fetchChassisInfo(self, vid, tag):
+        chassis = self.fetchVehicleInfo(vid)['chassis']
+        return chassis[tag]
+
+    def fetchTurretInfo(self, vid, tag):
+        turret = self.fetchVehicleInfo(vid)['turret']
+        return turret[tag]
+
+    def fetchGunInfo(self, vid, tag):
+        gun = self.fetchVehicleInfo(vid)['gun']
+        return gun[tag]
+        
+    def fetchNationList(self):
+        return [ s.upper() for s in NATIONS ], NATIONS
+
+    def fetchTierList(self):
+        return TIERS, TIERS
+
+    def fetchTypeList(self):
+        return TYPES_LIST, TYPES_LIST
+
+    def fetchVehicleList(self, nation, tier, type):
+        list = [ v for v in self.__strage.values() if v['nation'] == nation and v['tier'] == tier and v['type'] == type ]
+        list = sorted(list, key=lambda x: x['order'])
+        return [ v['name'] for v in list ], [ v['id'] for v in list ]
+
+    def fetchChassisList(self, vid):
+        vehicleInfo = self.fetchVehicleInfo(vid)
+        labels = [ 'chassis: ' + vehicleInfo['chassis'][tag]['name'] for tag in vehicleInfo['chassisList'] ]
+        return [ labels, vehicleInfo['chassisList'] ]
+
+    def fetchTurretList(self, vid):
+        vehicleInfo = self.fetchVehicleInfo(vid)
+        labels = [ 'turret: ' + vehicleInfo['turret'][tag]['name'] for tag in vehicleInfo['turretList'] ]
+        return [ labels, vehicleInfo['turretList'] ]
+
+    def fetchGunList(self, vid):
+        vehicleInfo = self.fetchVehicleInfo(vid)
+        labels = [ 'gun: ' + vehicleInfo['gun'][tag]['name'] for tag in vehicleInfo['gunList'] ]
+        return [ labels, vehicleInfo['gunList'] ]
+
+g_strage = Strage()
+
+if __name__ == '__main__':
+    import io, sys
+    sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+    print(g_strage.fetchVehicleList('germany', '7', 'TD'))
+    print(g_strage.fetchVehicleInfo('germany/G18_JagdPanther'))
+    print(g_strage.fetchGunList('germany/G18_JagdPanther'))
+    print(g_strage.fetchGunInfo('germany/G18_JagdPanther', '_88mm_PaK_36_L56'))
+    print(g_strage.fetchGunInfo('germany/G18_JagdPanther', '_75mm_StuK42_L70'))
+    #print(g_strage.fetchVehicleInfo('germany/G110_Typ_205'))
+    #print(g_strage.fetchGunInfo('germany/G110_Typ_205', '_128mm_KwK44_L55'))
