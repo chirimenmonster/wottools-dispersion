4a3d4e1fee126be87a1913d803e6af6b47e3895e
chirimenmonster <chirimenmonster@users.noreply.github.com>
2020-01-31 18:33:28 +0900

    wip

--- lib/vpath.py
+++ lib/vpath.py
@@ -1,5 +1,15 @@
 
+import logging
+import io
 import re
+import zipfile
+from collections import namedtuple
+import xml.etree.ElementTree as ET
+
+from lib.XmlUnpacker import XmlUnpacker
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 _config = {
     'vehicles': {
@@ -14,30 +24,34 @@ _config = {
     }
 }
 
-class _RePattern(object):
-    def __init__(self, pattern=None, path=None, pkg=None):
-        self.pattern = pattern
-        self.path = path
-        self.pkg = pkg
-        self.dir = None
+PathInfo = namedtuple('PathInfo', 'path pkg', defaults=(None,))
+
 
 class VPath(object):
 
-    def __init__(self, basedir=None, pkgpath=None, scriptsdir=None, guidir=None):
-        self.__basedir = basedir
-        self.__pkgpath = pkgpath
+    RePattern = namedtuple('RePattern', 'pattern path pkg pkgdir dir', defaults=(None, None))
+
+    def __init__(self, pkgdir=None, scriptsdir=None, guidir=None, scriptspkg=None, guipkg=None):
         self.__pattern = []
+        self.__cachedData = {}
+        self.__cachedXml = {}
         for k,v in _config.items():
-            p = _RePattern(**v)
-            if scriptsdir and p.pkg == 'scripts.pkg':
-                p.pkg = None
-                p.dir = scriptsdir
-            elif guidir and p.pkg == 'gui.pkg':
-                p.pkg = None
-                p.dir = guidir
+            p = self.RePattern(**v)
+            if pkgdir:
+                p = p._replace(pkgdir=pkgdir)
+            if p.pkg == 'scripts.pkg':
+                if scriptsdir:
+                    p = p._replace(pkg=None, dir=scriptsdir)
+                elif scriptspkg:
+                    p = p._replace(pkgdir=None, pkg=scriptspkg)
+            elif p.pkg == 'gui.pkg':
+                if guidir:
+                    p = p._replace(pkg=None, dir=guidir)
+                elif guipkg:
+                    p = p._replace(pkgdir=None, pkg=guipkg)
             self.__pattern.append(p)
 
-    def pkgpath(self, target):
+    def getPathInfo(self, target):
         rpath = target
         for p in self.__pattern:
             (rpath, n) = re.subn(p.pattern, p.path, rpath)
@@ -47,31 +61,77 @@ class VPath(object):
         assert n == 1, rpath
         if p.dir:
             filepath = '/'.join([ p.dir, rpath ])
-            result = { 'path':filepath }
+            result = PathInfo(filepath)
         else:
-            pkgpath = '/'.join([ self.__basedir, self.__pkgpath, pkg ])
-            result = { 'pkg':pkgpath, 'rpath':rpath }
+            pkgpath = '/'.join([ p.pkgdir, pkg ]) if p.pkgdir else pkg
+            result = PathInfo(rpath, pkgpath)
         return result
-        
 
-def readData(domain, target=None):
-    vpath = config.DATA[domain]['vpath'] + ('/' + target if target else '')
 
-    if config.DATA[domain]['extracted']:
-        path = '/'.join([ config.DATA[domain]['extracted'], vpath.split('/', 1)[1] ])
+class Strage(object):
+
+    def __init__(self):
+        self.__cachedData = {}
+        self.__cachedXml = {}
+
+    def readStream(self, path, pkg=None):
+        if not isinstance(path, PathInfo):
+            path, file = PathInfo(path, pkg), path
+        else:
+            file, pkg = path
+        if pkg:
+            try:
+                with zipfile.ZipFile(pkg, 'r') as zip:
+                    with zip.open(file, 'r') as fp:
+                        stream = io.BytesIO(fp.read())
+            except FileNotFoundError:
+                raise FileNotFoundError('pkgfile not found: {}'.format(pkg))
+            except KeyError:
+                raise KeyError('file not found: {}, in pkgfile: {}'.format(file, pkg))
+        else:
+            try:
+                with open(file, 'rb') as fp:
+                    stream = io.BytesIO(fp.read())
+            except FileNotFoundError:
+                raise FileNotFoundError('cannot open file: {}'.format(file))
+        return stream
+
+    def readData(self, path, pkg=None):
+        if not isinstance(path, PathInfo):
+            path, file = PathInfo(path, pkg), path
+        if path in self.__cachedData:
+            return self.__cachedData[path]
+        data = self.readStream(path).read()
+        self.__cachedData[path] = data
+        return data
+
+    def readXml(self, path, pkg=None):
+        if not isinstance(path, PathInfo):
+            path, file = PathInfo(path, pkg), path
+        if path in self.__cachedXml:
+            return self.__cachedXml[path]
+        xmlunpacker = XmlUnpacker()
+        stream = self.readStream(path)
         try:
-            with open(path, 'rb') as file:
-                stream = io.BytesIO(file.read())
-        except:
-            logger.debug('cannot open file: {}'.format(path))
-            raise
-    else:
-        pkgpath = '/'.join([ config.BASE_DIR, config.PKG_RELPATH, config.DATA[domain]['packed'] ])
-        try:
-            with zipfile.ZipFile(pkgpath, 'r') as zip:
-                with zip.open(vpath, 'r') as data:
-                    stream = io.BytesIO(data.read())
-        except:
-            logger.debug('cannot open vfile: {}, {}'.format(pkgpath, vpath))
-            raise
-    return stream
+            root = xmlunpacker.read(stream)
+        except ET.ParseError:
+            stream.seek(0)
+            data = stream.read()
+            data = re.sub(r'<xmlns:xmlref>.*?</xmlns:xmlref>', r'', data.decode('utf-8')).encode('utf-8')
+            try:
+                root = xmlunpacker.read(io.BytesIO(data))
+            except:
+                logger.error('cannot parse file: {}'.format(path))
+                raise
+        self.__cachedXml[path] = root
+        return root
+
+    def isCachedData(self, path, pkg=None):
+        if not isinstance(path, PathInfo):
+            path = PathInfo(path, pkg)
+        return path in self.__cachedData
+
+    def isCachedXml(self, path, pkg=None):
+        if not isinstance(path, PathInfo):
+            path = PathInfo(path, pkg)
+        return path in self.__cachedXml
--- test/test_vpath.py
+++ test/test_vpath.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import unittest
+import xml.etree.ElementTree as ET
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -9,36 +10,81 @@ from lib import vpath as vp
 
 
 class VPathTestCase(unittest.TestCase):
-    def test_vpath(self):
-        vpath = vp.VPath(basedir='c:/Games/World_of_Tanks_ASIA', pkgpath='res/packages')
-        self.assertEqual({
-            'pkg':      'c:/Games/World_of_Tanks_ASIA/res/packages/scripts.pkg',
-            'rpath':    'scripts/item_defs/vehicles/ussr/list.xml'
-        }, vpath.pkgpath('vehicles/ussr/list.xml'))
-        self.assertEqual({
-            'pkg':      'c:/Games/World_of_Tanks_ASIA/res/packages/gui.pkg',
-            'rpath':    'gui/gui_settings.xml'
-        }, vpath.pkgpath('gui/gui_settings.xml'))
+
+    def test_vpath_getPathInfo(self):
+        vpath = vp.VPath(pkgdir='c:/Games/World_of_Tanks_ASIA/res/packages')
+        self.assertEqual(vp.PathInfo(
+            pkg='c:/Games/World_of_Tanks_ASIA/res/packages/scripts.pkg',
+            path='scripts/item_defs/vehicles/ussr/list.xml'
+        ), vpath.getPathInfo('vehicles/ussr/list.xml'))
+        self.assertEqual(vp.PathInfo(
+            pkg='c:/Games/World_of_Tanks_ASIA/res/packages/gui.pkg',
+            path='gui/gui_settings.xml'
+        ), vpath.getPathInfo('gui/gui_settings.xml'))
 
     def test_vpath_scriptsdir(self):
-        vpath = vp.VPath(basedir='c:/Games/World_of_Tanks_ASIA', pkgpath='res/packages', scriptsdir='d:/git/wot.scripts')
-        self.assertEqual({
-            'path':     'd:/git/wot.scripts/scripts/item_defs/vehicles/ussr/list.xml'
-        }, vpath.pkgpath('vehicles/ussr/list.xml'))
-        self.assertEqual({
-            'pkg':      'c:/Games/World_of_Tanks_ASIA/res/packages/gui.pkg',
-            'rpath':    'gui/gui_settings.xml'
-        }, vpath.pkgpath('gui/gui_settings.xml'))
+        vpath = vp.VPath(pkgdir='c:/Games/World_of_Tanks_ASIA/res/packages', scriptsdir='d:/git/wot.scripts')
+        self.assertEqual(vp.PathInfo(
+            path='d:/git/wot.scripts/scripts/item_defs/vehicles/ussr/list.xml'
+        ), vpath.getPathInfo('vehicles/ussr/list.xml'))
+        self.assertEqual(vp.PathInfo(
+            pkg='c:/Games/World_of_Tanks_ASIA/res/packages/gui.pkg',
+            path='gui/gui_settings.xml'
+        ), vpath.getPathInfo('gui/gui_settings.xml'))
 
     def test_vpath_guidir(self):
-        vpath = vp.VPath(basedir='c:/Games/World_of_Tanks_ASIA', pkgpath='res/packages', guidir='./test')
-        self.assertEqual({
-            'pkg':      'c:/Games/World_of_Tanks_ASIA/res/packages/scripts.pkg',
-            'rpath':    'scripts/item_defs/vehicles/ussr/list.xml'
-        }, vpath.pkgpath('vehicles/ussr/list.xml'))
-        self.assertEqual({
-            'path':     './test/gui/gui_settings.xml'
-        }, vpath.pkgpath('gui/gui_settings.xml'))
+        vpath = vp.VPath(pkgdir='c:/Games/World_of_Tanks_ASIA/res/packages', guidir='./test')
+        self.assertEqual(vp.PathInfo(
+            pkg='c:/Games/World_of_Tanks_ASIA/res/packages/scripts.pkg',
+            path='scripts/item_defs/vehicles/ussr/list.xml'
+        ), vpath.getPathInfo('vehicles/ussr/list.xml'))
+        self.assertEqual(vp.PathInfo(
+            path='./test/gui/gui_settings.xml'
+        ), vpath.getPathInfo('gui/gui_settings.xml'))
+
+
+class StrageTestCase(unittest.TestCase):
+
+    def test_strage_readStream(self):
+        file = 'test/data/res/gui/gui_settings.xml'
+        strage = vp.Strage()
+        with open(file, 'rb') as fp:
+            data = fp.read()
+        self.assertEqual(data, strage.readStream(file).read())
+        with self.assertRaises(FileNotFoundError):
+            strage.readStream('missing')
+        with self.assertRaises(KeyError):
+            strage.readStream('missing', 'test/data/empty.zip')
+        with self.assertRaises(FileNotFoundError):
+            strage.readStream('missing', 'test/data/missing.zip')
+
+    def test_strage_readData(self):
+        file = 'test/data/res/gui/gui_settings.xml'
+        strage = vp.Strage()
+        with open(file, 'rb') as fp:
+            data = fp.read()
+        self.assertFalse(strage.isCachedData(file))
+        self.assertEqual(data, strage.readData(file))
+        self.assertTrue(strage.isCachedData(file))
+        self.assertEqual(data, strage.readData(file))
+
+
+    def test_vpath_readXml(self):
+        file = 'test/data/res/gui/gui_settings.xml'
+        strage = vp.Strage()
+        path = vp.PathInfo(file)
+        with open(file, 'rb') as fp:
+            data = ET.tostring(ET.fromstring(fp.read()), encoding='utf-8')
+        self.assertFalse(strage.isCachedXml(file))
+        self.assertEqual(data, ET.tostring(strage.readXml(file), encoding='utf-8'))
+        self.assertTrue(strage.isCachedXml(path))
+        with self.assertRaises(FileNotFoundError):
+            strage.readXml('missing.xml')
+        with self.assertRaises(KeyError):
+            strage.readXml('missing', 'test/data/empty.zip')
+        element = strage.readXml('test/data/badnamespace.xml')
+        self.assertEqual('#usa_vehicles:T14', element.find('A21_T14/userString').text)
+
 
 if __name__ == '__main__':
     unittest.main()
