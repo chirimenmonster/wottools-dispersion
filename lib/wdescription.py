f0cf8da92881831cbbfd714f66d35f2639db4021
chirimenmonster <chirimenmonster@users.noreply.github.com>
2020-02-26 18:21:08 +0900

    wip

--- lib/wdescription.py
+++ lib/wdescription.py
@@ -1,4 +1,5 @@
 
+import math
 import string
 from collections import namedtuple
 import tkinter.ttk
@@ -66,7 +67,7 @@ class SpecViewItem(tkinter.Frame):
             widget.pack(side='left')
 
     def update(self, isNone=False):
-        #self.pack_forget()
+        self.pack_forget()
         if isNone and self.isPhantom:
             return
         #self.pack(side='top', fill='x', expand=1)
@@ -116,6 +117,7 @@ class ValueItem(tkinter.Entry):
 class ValueTextItem(tkinter.Text):
     def __init__(self, *args, app=None, desc=None, **kwargs):
         super(ValueTextItem, self).__init__(*args, **kwargs, height=4)
+        self.font = tkinter.font.Font(family='Arial', size=10, weight='normal')
         self.app = app
         template = '{}'
         stringvar = tkinter.StringVar(value='')
@@ -124,8 +126,62 @@ class ValueTextItem(tkinter.Text):
         self.app.vehicleStatsPool.add(desc['value'], '{}', stringvar, self)
         self.delete('1.0', 'end')
         self['height'] = 1
-        self.lock = True
-        self['yscrollcommand'] = self.setScroll
+        #self.lock = True
+        #self['yscrollcommand'] = self.setScroll
+        self.__nrows = tkinter.StringVar(value='')
+        #self.__nrows.trace('w', self.delayResize)
+        self.bind('<Map>', self.catchMap)
+        self['yscrollcommand'] = self.dummyScrollCommand
+
+    def catchMap(self, *args):
+        print('catchMap: change height=1', flush=True)
+        self.__nrows.set(value='')
+        #self['height'] = 1
+        #self.resizeHeight()
+        
+        
+    def delayResize(self, *args):
+        print('derayResize:', flush=True)
+        self.after(5000, self.resizeHeight)
+   
+    
+    def resizeHeight(self, *args):
+        bwidth = float(self['width']) * 7
+        width = 0
+        nlines = 0
+        for line in self.get('1.0', 'end-1c').split('\n'):
+            width = max(width, self.font.measure(line))
+            nlines += math.ceil(width / bwidth)
+            print('width={}, bwidth={},  nlines={}'.format(width, bwidth, nlines))
+        nrows = nlines
+        #nrows = self.__nrows.get()
+        #nrows = self.__needsRows
+        print('resizeHeight: nrows={}'.format(repr((nrows))), flush=True)
+        self['height'] = nrows
+        #if nrows != '':
+        #    print('resizeHeight: set height, nrows={}'.format(repr((nrows))), flush=True)
+        #    self['height'] = nrows
+        #else:
+        #    print('resizeHeight: unknown nrows={}'.format(repr((nrows))), flush=True)
+        #self['height'] = 1
+        #self['yscrollcommand'] = None
+        #self['height'] = self.__nrows
+
+    def getDisplayHeight(self):
+        self.__nrows.set('')
+        self['height'] = 1
+        print('getDisplayHeight', flush=True)
+
+    def dummyScrollCommand(self, first, last):
+        first, last = float(first), float(last)
+        nrows = self['height'] / (last - first)
+        self.__needsRows = nrows
+        #current = self.__nrows.get()
+        print('dummyScrollCommand: need={}, height={}, first={}, last={}'.format(nrows, self['height'], first, last), flush=True)
+        #if current != '':
+        #    return
+        #self.__nrows.set(nrows)
+        #print('dummyScrollCommand: need={}, current={}'.format(nrows, self['height']), flush=True)
 
     def setScroll(self, first, last):
         if self.lock:
@@ -141,8 +197,17 @@ class ValueTextItem(tkinter.Text):
 
     def callback(self, *args):
         text = self.__stringvar.get()
+        print('callback: len={}, text={}...'.format(len(text), text[:16]), flush=True)
         self.delete('1.0', 'end')
-        self['height'] = 1
-        self.lock = False
+        #self['height'] = 1
+        #self.lock = False
         self.insert('1.0', text)
+        #self.resize()
         self.master.update(isNone=(text == ''))
+        #self.__nrows.set('')
+        #self['height'] = 1
+        #self.delayResize()
+        #self.after(1000, lambda : self.yview_moveto(0.0))
+        #self.after(2000, lambda : self.yview_moveto(1.0))
+        #self.after(1000, lambda : self.resizeHeight())
+        self.resizeHeight()
